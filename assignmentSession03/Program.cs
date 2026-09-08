23ecff6904236f031e1528a54f56dd11e73c0810
YoumnaE <yomnaelsayed3@gmail.com>
2026-09-08 21:42:12 +0300

    question 1 and 2

--- assignmentSession03/Program.cs
+++ assignmentSession03/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            #region ToUpper
+            string title = "clean code";
+            string upperTitle = title.ToUpper();
+
+            Console.WriteLine(title);
+            Console.WriteLine(upperTitle);
+
+            #endregion
+
+
+            #region
+
+            string string1 = "Clean Code";
+            string string2 = "Clean Code";
+            string string3 = string1;
+
+            #endregion
+
+            Console.ReadLine();
         }
     }
 }
